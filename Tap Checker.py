3db61c789c26c38c61742abf60b507d974776a70
nick-burrill <76707213+nick-burrill@users.noreply.github.com>
2022-02-05 17:58:58 -0500

    Upload new Tap Checker

--- Tap Checker.py
+++ Tap Checker.py
@@ -1,112 +1,58 @@
-from rich.console import Console
-from rich.table import Table
 from rich import print
-from decimal import *
-import os, sys
 
-try:
-    droppedFile = sys.argv[1:]
-except IndexError:
-    print("No file selected")
+# TODO: Drag drop file loading
+#       Load file by launching with arguments
+#       Pitch calculation
+#       Pretty output table
 
-for p in droppedFile:
-    with open(p, 'r') as file:
-        lines = file.readlines()
+filePath = "Sample Gcode/Solidworks/CGIP11719.NC"
 
-manual_file=False
-filename= "anothertest.nc"
-if manual_file == True:
-    with open(filename, 'r') as file:
-        lines = file.readlines()
+with open(filePath, 'r') as file:
+    lines = file.readlines()
 
+def readLine(line):
+    codeList = ['G','M','T','S','F','X','Y','Z','H','D','N','R','P']
+    newWord = ""
+    wordList = []
+    for i, char in enumerate(line):
+        if char in codeList or char == "\n" or char == " ":
+            if newWord != "" and newWord != " ":
+                wordList.append(newWord)
+            newWord = ""
+        newWord += char
+    return(wordList)
 
-# Determine if each tool change has a tapping cycle or not
-rigidTappingIndexes = []
+def readWords(words, values):
+    for word in words:
+        letter = word[0] # Separate key letter from value
+        value = word[1:]
+
+        if letter == 'F':
+            if values["feed"] == False:
+                values["feed"] = value
+        elif letter == 'S':
+            if values["rpm"]== False:
+                values["rpm"] = value
+        elif letter == 'T':
+            if values["tool"] == False:
+                values["tool"] = value
+        # print((letter,value))
+    return(values)
+
+tappingCycleIndex = []
 for lineIndex, line in enumerate(lines):
     if line.find("G84") != -1:
-        rigidTappingIndexes.append(lineIndex+1)
-        # print(lineIndex, line)
-# print("rigid tapping indexes:", rigidTappingIndexes)
+        tappingCycleIndex.append(lineIndex+1)
 
+allValues = []
 
-allData = []
+for i, tappingCycle in enumerate(tappingCycleIndex):
+    valueDict = {"feed":False, "rpm":False, "tool":False}
+    lineToRead = tappingCycle
+    while False in valueDict.values():
+        words = readLine(lines[lineToRead])
+        readWords(words, valueDict)
+        lineToRead -= 1
+    allValues.append(valueDict)
 
-for tappingCycle in rigidTappingIndexes:
-    feedPerRev = False
-    Feed = 0
-    FeedFound = False
-    RPM = 0
-    RPMFound = False
-    Tool = 0
-    toolFound = False
-    
-    readLine = tappingCycle
-
-    while lines[readLine].find("M06") == -1:
-        # print(lines[readLine])
-        readLine = readLine - 1
-
-        if lines[readLine].find("G95") != -1:
-            feedPerRev = True
-
-        if lines[readLine].find("F") != -1 and FeedFound == False:
-            Feed = lines[readLine][lines[readLine].index('F')+1:].strip()
-            Feed = Decimal(Feed)
-            FeedFound = True
-
-        if lines[readLine].find("S") != -1 and RPMFound == False:
-            RPM = lines[readLine][lines[readLine].index('S')+1:].strip()
-            # print(RPM)
-            for word in RPM.split():
-                if word.isdigit():
-                    RPM = word
-            RPM = Decimal(RPM)
-            RPMFound = True
-
-        if lines[readLine].find("T") != -1 and toolFound == False:
-            Tool = lines[readLine][lines[readLine].index('T')+1:].strip()
-            Tool = Tool.replace("M06",'')
-            Tool = int(Tool)
-            toolFound = True
-
-    # Pitch Calculation
-    if feedPerRev == True:
-        if Feed != 0:
-            iPitch = Feed
-            mPitch = Feed*25.4
-        else:
-            iPitch, mPitch = "N/A","N/A"
-
-    else:
-        if Feed != 0 and RPM != 0:
-            iPitch = RPM/Feed
-            mPitch = Feed/RPM*Decimal("25.4")
-        else:
-            iPitch, mPitch = "N/A","N/A"
-
-    # Convert all data into strings for table printing later
-    data = {"ToolNum": Tool,
-            "RPM":str(RPM),
-            "Feed":str(Feed),
-            "Feed/Rev":feedPerRev,
-            "Metric Pitch": str(mPitch),
-            "Imperial Pitch": str(iPitch)}
-    allData.append(data)
-
-
-# Sort data
-allData = sorted(allData, key= lambda i: i['ToolNum'])
-
-# Print Results
-console = Console()
-table = Table(show_header=True)
-table.add_column("Tool")
-table.add_column("RPM")
-table.add_column("Feed")
-table.add_column("Metric Pitch")
-table.add_column("Imperial Pitch")
-for tool in allData:
-    table.add_row(str(tool["ToolNum"]),tool["RPM"],tool["Feed"],tool["Metric Pitch"],tool["Imperial Pitch"])
-console.print(table)
-
-input("Press enter to quit...")
+print(allValues)
